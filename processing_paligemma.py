e553043b6c234ea091279cb544e56ed704377fb5
adityakak27 <146877885+adityakak27@users.noreply.github.com>
2024-09-14 15:22:18 +0530

    Update processing_paligemma.py
    Added processing methods for the main 'PaliGemmaProcessor' class

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -6,7 +6,59 @@ from PIL import Image
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
+def rescale(
+        image: np.ndarray, scale : float, dtype : np.dtype = np.float32
+    ) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
 
+    return rescaled_image
+
+def resize(
+        image : Image,
+        size : Tuple[int, int],
+        resample : Image.Resampling = None,
+        reducing_gap : Optional[int] = None,
+) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize(
+        (width, height), resample = resample, reducing_gap = reducing_gap
+    )
+    return resized_image
+
+def normalize(
+        image : np.ndarray,
+        mean : Union[float, Iterable[float]],
+        std : Union[float, Iterable[float]]
+) -> np.ndarray:
+    mean = np.array(mean, dtype= image.dtype)
+    std = np.array(std, dtype= image.dtype)
+
+    image = (image - mean) / std
+    return image
+
+
+def process_images(
+        image : List[Image.Image],
+        size : Dict[str, int] = None,
+        resample : Image.Resampling = None,
+        rescale_factor : float = None,
+        image_mean : Optional[Union[float, List[float]]] = None,
+        image_std : Optional[Union[float, List[float]]] = None,
+) -> List[np.ndarray]:
+    height, width = size[0], size[1]
+    images = [resize(image = image, size = (height, width), resample = resample) for image in images]
+
+    #convert each to an np array
+    images = [np.array(image) for image in images]
+    #rescale pixel values to be between 0 and 1
+    images = [rescale(image, scale = rescale_factor) for image in images]
+    #normalize to have mean 0 and std dev 1
+    images = [normalize(image, mean = image_mean, std = image_std) for image in images]
+    #change the dimensions as per the model expectations ([])
+    images = [image.transpose(2, 0, 1) for image in images]
+
+    return images
 
 #for this processor, the image size is 224x224, and the model generates 128 tokens for the text description
 
